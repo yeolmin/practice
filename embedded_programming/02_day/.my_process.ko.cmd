6b2db843c588c13c9731e57db790f9b892732cd4
jikim <jikim@imguru.co.kr>
2018-07-10 16:43:14 +0900

    ?

---
+++ embedded_programming/02_day/.my_process.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/practice/embedded_programming/02_day/my_process.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /root/practice/embedded_programming/02_day/my_process.ko /root/practice/embedded_programming/02_day/my_process.o /root/practice/embedded_programming/02_day/my_process.mod.o ;  true
---
+++ embedded_programming/02_day/.tmp_versions/my_process.mod
@@ -0,0 +1,3 @@
+/root/practice/embedded_programming/02_day/my_process.ko
+/root/practice/embedded_programming/02_day/my_process.o
+
